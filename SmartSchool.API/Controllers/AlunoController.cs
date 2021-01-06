5a735ca5c0bc88d2945bb952747ced238c8b0390
Vinicius9-Nunes <viniciusonunes9@gmail.com>
2021-01-06 14:36:46 -0300

    Implementando os verbos e testando

--- SmartSchool.API/Controllers/AlunoController.cs
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
 using SmartSchool.API.Models;
 using System;
 using System.Collections.Generic;
@@ -13,36 +15,99 @@ namespace SmartSchool.API.Controllers
     [ApiController]
     public class AlunoController : ControllerBase
     {
+
+        private readonly DataContext _context;
+
+        public AlunoController(DataContext context)
+        {
+            _context = context;
+        }
+
+
         // GET: api/<AlunoController>
         [HttpGet]
-        public IEnumerable<Aluno> Get()
+        public IActionResult Get()
         {
-            return new List<Aluno>() { };
+            return Ok(_context.Alunos);
         }
 
         // GET api/<AlunoController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var aluno = _context.Alunos.FirstOrDefault(p => p.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+            return Ok(aluno);
+        }
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            if (!string.IsNullOrEmpty(nome) && !string.IsNullOrEmpty(sobrenome))
+            {
+                var filtro = _context.Alunos.FirstOrDefault(p => p.Nome == nome && p.Sobrenome == sobrenome);
+                if (filtro == null) return BadRequest("Aluno não localizado com os parametros informados!");
+                return Ok(filtro);
+            }
+            else if (string.IsNullOrEmpty(nome)) return BadRequest("Informe um nome");
+            else
+            {
+                var filtro = _context.Alunos.FirstOrDefault(p => p.Nome == nome);
+                if (filtro == null) return BadRequest("Ocorreu um erro");
+                return Ok(filtro);
+            }
         }
 
         // POST api/<AlunoController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post(Aluno aluno)
         {
+            if (aluno == null) return BadRequest("Favor informar um aluno");
+            _context.Add(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
         }
 
         // PUT api/<AlunoController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, Aluno aluno)
         {
+            if (id == 0) return BadRequest("Favor informar o Id do aluno");
+            if (aluno == null) return BadRequest("Favor informar um aluno");
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (alu == null) return BadRequest("Aluno não encontrado");
+            if (alu.Id != id) return BadRequest("Id informado não corresponde ao Id encontrado no Banco");
+            if (aluno.Id == 0)
+                aluno.Id = alu.Id;
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            if (id == 0) return BadRequest("Favor informar o Id do aluno");
+            if (aluno == null) return BadRequest("Favor informar um aluno");
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (alu == null) return BadRequest("Aluno não encontrado");
+            if (alu.Id != id) return BadRequest("Id informado não corresponde ao Id encontrado no Banco");
+            if (aluno.Id == 0)
+                aluno.Id = alu.Id;
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
         }
 
         // DELETE api/<AlunoController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            if (id == 0) return BadRequest("Favor informar o Id do aluno");
+            var aluno = _context.Alunos.FirstOrDefault(p => p.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado.");
+            _context.Remove(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
         }
     }
 }
--- SmartSchool.API/Controllers/ProfessorController.cs
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,36 +14,78 @@ namespace SmartSchool.API.Controllers
     [ApiController]
     public class ProfessorController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ProfessorController(DataContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/<ProfessorController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var prof = _context.Professores;
+            if (prof == null) return BadRequest("Não há nenhum professor");
+            return Ok(prof);
         }
 
         // GET api/<ProfessorController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            if (id == 0) return BadRequest("Favor informe um Id valido.");
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            return Ok(prof);
         }
 
         // POST api/<ProfessorController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Professor professor)
         {
+            if (professor == null) return BadRequest("Informe um professor.");
+            _context.Add(professor);
+            _context.SaveChanges();
+            return Ok(professor);
         }
 
         // PUT api/<ProfessorController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Professor professor)
         {
+            if (id == 0) return BadRequest("Favor informe um Id");
+            if (professor == null) return BadRequest("Informe um professor.");
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (prof == null) return BadRequest("Professor não encontrado");
+            professor.Id = prof.Id;
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] Professor professor)
+        {
+            if (id == 0) return BadRequest("Favor informe um Id");
+            if (professor == null) return BadRequest("Informe um professor.");
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (prof == null) return BadRequest("Professor não encontrado");
+            professor.Id = prof.Id;
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
         }
 
         // DELETE api/<ProfessorController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            if (id == 0) return BadRequest("Favor informe um Id");
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (prof == null) return BadRequest("Professor não encontrado");
+            _context.Remove(prof);
+            _context.SaveChanges();
+            return Ok("Professor deletado: " + prof);
         }
     }
 }
